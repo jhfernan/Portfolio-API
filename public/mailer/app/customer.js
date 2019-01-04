dbbb959be2c23324ed9b66301d1de7edb29d9480
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-01-04 12:19:49 -0500

    Updated color for mailer

--- public/mailer/app/customer.js
+++ public/mailer/app/customer.js
@@ -10,10 +10,10 @@ let response = info => {
 							<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 							<title>Contact Submission</title>
 						</head>
-						<body class="pt-5" style="background-color: #9C27B0;">
+						<body class="pt-5" style="background-color: #607D8B;">
 							<div class="container pb-3 text-center">
 								<div class="jumbotron bg-white">
-									<h1 style="color: #7B1FA2;">Hi ${ info.name },</h1>
+									<h1 style="color: #455A64;">Hi ${ info.name },</h1>
 									<p class="lead">Thank you for taking a moment to contact me about my portfolio website!</p>
 									<p class="lead">I will be looking over submission and will be in touch shortly.<br />I look forward to talking to you soon!</p>
 									<p class="lead">Best regards,<br />Jonathan Fernandes</p>
